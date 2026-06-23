8cfadc244b3840637613c0fbb45c539a84796fae
Anik Karmakar <anikkarmakar@Aniks-MacBook-Air.local>
2026-06-24 00:56:54 +0530

    Use Docker Hub image

--- app/main.py
+++ app/main.py
@@ -1,19 +1,8 @@
-import os
 from dotenv import load_dotenv
 from fastapi import FastAPI
 
-load_dotenv()
 app = FastAPI()
 
-DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_PORT")
-
-@app.get("/config")
-def config():
-    return {
-        "db_host": DB_HOST,
-        "db_port": DB_PORT
-    }
 
 @app.get("/")
 def home():
